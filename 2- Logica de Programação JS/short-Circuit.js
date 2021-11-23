f36b90b00411f000852f5cf2778ad862a47d0a60
liafarias <liacarlos@alu.ufc.br>
2021-11-23 09:32:59 -0300

    if, else if, else

--- 2- Logica de Programação JS/short-Circuit.js
+++ 2- Logica de Programação JS/short-Circuit.js
@@ -1,28 +1,16 @@
 /**
  * && -> false && true -> false "o valor mesmo"
- * || -> true || false -> Vai retornar "o valor verdadeiro"
+ * || ->
  * VALORES QUE AVALIAM EM FALSO QUANDO NECESSARIO(FALSY): false, 0, ' ', null, undefined, NaN 
  */
 
-// EXEMPLOS AND
+// EXEMPLOS
 console.log('Luiz' && 'Maria'); // TRUE - retorna o ultimo valor checado ('Maria').
-console.log('Luiz' && NaN && 'Maria'); // FALSE - retorna o ultimo valor falso checados (Nan).
+console,log('Luiz' && ' ' && 'Maria'); // FALSE - retorna o(s) valor(es) falsos checados (' ').
 
 // EXEMPLO PRATICO
 function falarOi(){
-    return 'oi';
+    return 'Oi';
 }
-let vaiExecutar = 'Joãozinho';
+const vaiExecutar = false;
 console.log(vaiExecutar && falarOi());
-
-// EXEMPLOS OR
-console.log(0 || false || null || 'Luiz Otavio' || true); // TRUE - Retorna o primeiro valor verdadeiro ('Luiz Otavio)
-console.log(0 || false || null || true);
-
-// EXEMPLO PRATICO
-let corUsuario = null;
-const corPadrao = corUsuario || 'preto';
-console.log(corPadrao); // retorna a cor padrão pois o usuario não escolheu uma cor (null é um valor falso então retorna preto que seria um valor verdadeiro)
-corUsuario = 'vermelho';
-const corPadrao2 = corUsuario || 'preto';
-console.log(corPadrao2); // retorna a cor escolhida pelo usuario pois existem duas informações verdadeiras e será retornada apenas a primeira, que no caso é a let "vermelha";
