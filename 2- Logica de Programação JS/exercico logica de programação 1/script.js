6d99c745a73bbe63e4b24f7040c6ab28e6b716cc
liafarias <liacarlos@alu.ufc.br>
2021-11-28 19:49:05 -0300

    Exercicio Logica de programação 1

--- 2- Logica de Programação JS/exercico logica de programação 1/script.js
+++ 2- Logica de Programação JS/exercico logica de programação 1/script.js
@@ -2,8 +2,25 @@
 
 function compararDoisNumeros (num1, num2){
     if(num1>num2){
-        return num1;
+        return(num1);
     } else if(num2>num1){
-        console.log(num2)
+        return(num2)
     }
 }
+console.log(compararDoisNumeros(5,8))
+
+
+// OUTROS MODOS
+
+function max (x, y){
+    if (x > y) return x;
+    return y
+}
+
+console.log(max(10, 20));
+
+function max2 (a,b){
+    return a > b ? a : b;
+}
+
+console.log(max2 (4,7));
